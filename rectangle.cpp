b3587ce6df2b58024e6a29253f638de4b4e588da
mohadeseheydari <mohadeseh1997@yahoo.com>
2016-06-24 02:35:10 +0430

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -3,6 +3,8 @@
 Rectangle::Rectangle()
 {
     setPixmap(QPixmap(":/images/Rectangle.png"));
+    //choose a location randomly
+    //set this size to not to go outside of bounds
     int rand1=rand()%720;
     int rand2=rand()%528;
     this->setPos(rand1,rand2);
