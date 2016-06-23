06a0d0b76edbd1fcc1ecc74b596343d08ba81765
mohadeseheydari <mohadeseh1997@yahoo.com>
2016-06-24 03:37:54 +0430

    Update bullet.cpp

--- bullet.cpp
+++ bullet.cpp
@@ -20,7 +20,7 @@ void Bullet::move()
     QList<QGraphicsItem *> itemList = collidingItems();
     for(int i=0 ;i < itemList.size();i++)
     {
-        if(typeid(*(itemList[i])) == typeid(player))\
+        if(typeid(*(itemList[i])) == typeid(player))
         {
             game->score->increase();
 
