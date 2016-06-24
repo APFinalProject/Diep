a348162d2b2e1da8528cfc9e693925161deffdc7
mohadeseheydari <mohadeseh1997@yahoo.com>
2016-06-24 15:42:42 +0430

    Delete rectangle.cpp

--- rectangle.cpp
+++
@@ -1,9 +0,0 @@
-#include "rectangle.h"
-
-Rectangle::Rectangle()
-{
-    setPixmap(QPixmap(":/images/Rectangle.png"));
-    int rand1=rand()%720;
-    int rand2=rand()%528;
-    this->setPos(rand1,rand2);
-}
