1f11662bb22ba60031015ceb5abca1eb3cf2f6ad
mohadeseheydari <mohadeseh1997@yahoo.com>
2016-06-24 16:04:12 +0430

    Update pentagon.cpp

--- pentagon.cpp
+++ pentagon.cpp
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 pentagon::pentagon()
 {
-    setPixmap(QPixmap(":/images/Rectangle.png"));
+    setPixmap(QPixmap(":/images/pentagon.png"));
     int rand1=rand()%749;
     int rand2=rand()%550;
     this->setPos(rand1,rand2);
