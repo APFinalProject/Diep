c846039391d8f75a004db5b5fe2f02fe7f87b07d
Sarah Ghanei <sarahghanei1997@gmail.com>
2016-06-24 23:23:38 +0430

    Add files via upload
    i've written a code that when our player collides with the objects such as triangle,square or pentagon it's health decreases.
    but this part of code resulted in error.
    i will change it soon

--- Player.cpp
+++ Player.cpp
@@ -1,10 +1,34 @@
 #include "Player.h"
 #include <QGraphicsPixmapItem>
-
+#include <game.h>
+#include <qmath.h>
+#include <math.h>
+#include "bullet.h"
+#include <QList>
+#include <typeinfo>
+#include "square.h"
+#include "triangle.h"
+#include "pentagon.h"
+extern Game * game;
 Player::Player(){
-setPixmap(QPixmap(":/pic1/tank (2).png"));
+    p_health=new Health();
+    p_health->setHealth(8);
+    setPixmap(QPixmap(":/pic1/tank (2).png"));
+    QTimer * timer=new QTimer();
+    QObject::connect(timer,SIGNAL(timeout()),this,SLOT(collide_player()));
+    timer->start(2000);
 
+}
+void Player::collide_player(){
+    QList<QGraphicsItem*> items=collidingItems();
+    for (int i=0,n=items.size();i<n;++i){
+        if(typeid(*(items[i]))== typeid(Square) ||typeid(*(items[i]))==typeid(Triangle)||typeid(*(items[i]))==typeid(Pentagon)){
+            // decrease the health of the player
+            Player().p_health->decrease ();
 
+        }
+    }
+    return;
 }
 void Player::keyPressEvent(QKeyEvent * event){
             if(event->key() == Qt::Key_Left)
@@ -36,6 +60,36 @@ void Player::keyPressEvent(QKeyEvent * event){
             else if(event->key()==Qt::Key_X){
                 setTransformOriginPoint(pixmap().width()/2,pixmap().height()*2/3);
                setRotation(rotation()+20);
+            }
+            else if(event->key() == Qt::Key_V)
+                {
+                    Bullet *bullet = new Bullet();
+
+
+
+                    if(fmod(rotation(), 360)<=0 && fmod(rotation(), 360)>-90
+                            || fmod(rotation(), 360)>=270 && fmod(rotation(), 360)<360)
+                        bullet->setPos(x()+ pixmap().width()/2 - bullet->pixmap().width()/3 + (( pixmap().height()*2/3/9)*(fmod(rotation(), 90)/10))
+                                   , y()- bullet->pixmap().height()/2 - (( pixmap().height()*2/3/9)*(fmod(rotation(), 90)/10)) );
+
+                    else if(fmod(rotation(), 360)<=-90 && fmod(rotation(), 360)>-180
+                            || fmod(rotation(), 360)>=180 && fmod(rotation(), 360)<270)
+                        bullet->setPos(x()- pixmap().width()/2 - bullet->pixmap().width()/3 - (( pixmap().height()*2/3/9)*(fmod(rotation(), 90)/10))
+                                       , y()+ bullet->pixmap().height()/2 - (( pixmap().height()*2/3/9)*(fmod(rotation(), 180)/10)));
+
+                    else if(fmod(rotation(), 360)>=90 && fmod(rotation(), 360)<180
+                            || fmod(rotation(), 360)<=-180 && fmod(rotation(), 360)>-270)
+                        bullet->setPos(x()+ pixmap().width()/2 + bullet->pixmap().width()/3 - ((pixmap().height()*2/3/8.5)*(fmod(rotation(), 90)/10))
+                                       , y()+pixmap().height());
+
+                    else if(fmod(rotation(), 360)>=0 && fmod(rotation(), 360)<90
+                            || fmod(rotation(), 360)<=-270 && fmod(rotation(), 360)>-360)
+                        bullet->setPos(x()+ pixmap().width()/3 - bullet->pixmap().width()/2 - (( pixmap().height()*2/3/9)*(fmod(rotation(), 90)/10))
+                                       , y()- bullet->pixmap().height()/2 - (( pixmap().height()*2/3/9)*((fmod(rotation(), 90)/10))));
+
+                    scene()->addItem(bullet);
+
+
             }
             return;
 
--- Player.h
+++ Player.h
@@ -3,13 +3,20 @@
 #include <QGraphicsPixmapItem>
 #include <QKeyEvent>
 #include <QWidget>
+#include <QList>
+#include <QTimer>
+#include <QObject>
+#include "health.h"
 
-
-
-class Player : public QGraphicsPixmapItem{
+class Player : public QObject,public QGraphicsPixmapItem{
+    Q_OBJECT
 public:
     Player();
     void keyPressEvent(QKeyEvent * event );
+    Health * p_health;
+
+public slots:
+    void collide_player();
 };
 
 #endif // PLAYER_H
